f1c48b2212c6bdc5787d571943278c525e2d2a98
Tillmann Schiffler <tillmann.schiffler@gmail.com>
2023-08-03 22:30:53 +0200

    Added test

---
+++ tests/unit/Infrastructure/JobreaderTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace unit\Infrastructure;
+
+use PHPUnit\Framework\TestCase;
+use simpleQueue\Infrastructure\Directory;
+use simpleQueue\Infrastructure\JobReader;
+
+/**
+ * @covers \simpleQueue\Infrastructure\JobReader
+ */
+class JobreaderTest extends TestCase
+{
+    public function testCanCreate(): void
+    {
+        $mockDir = $this->createMock(Directory::class);
+        
+        $this->assertInstanceOf(
+            JobReader::class,
+            new JobReader($mockDir)
+        );
+    }
+
+    public function testThrowExceptionOnInvalidJobFile(): void
+    {
+        $mockDir = $this->createMock(Directory::class);
+        $mockDir->expects($this->atLeastOnce())->method('toString')->willReturn(
+            __DIR__ . '/../Fixtures/'
+        );
+
+        $this->expectException(\InvalidArgumentException::class); 
+        (new JobReader($mockDir))->retrieveOldest();
+    }
+}
---
+++ tests/unit/Infrastructure/JsonTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace unit\Infrastructure;
+
+use PHPUnit\Framework\TestCase;
+use simpleQueue\Infrastructure\JobInfrastructureException;
+use simpleQueue\Infrastructure\Json;
+
+/**
+ * @covers \simpleQueue\Infrastructure\Json
+ */
+class JsonTest extends TestCase
+{
+    public function testCantCreateWithInvalidJsonString(): void
+    {
+        $this->expectException(JobInfrastructureException::class);
+        Json::fromString('sdf');
+    }
+
+    public function testCanEncode(): void
+    {
+        $this->assertEquals(
+            '{"foo":"bah"}',
+            Json::encode(['foo' => 'bah'])
+        );
+    }
+
+    public function testCanCreateWithValidJobData(): void
+    {
+        $this->assertInstanceOf(
+            Json::class,
+            Json::fromString(json_encode([
+                'jobId' => '1',
+                'jobPayload' => 'foo'
+            ]))            
+        );  
+    }
+}
