ed12e8917783dbdae50d7f28a63546b2f09a88a6
Tillmann Schiffler <tillmann.schiffler@gmail.com>
2024-02-11 17:26:21 +0100

    Draft: Keep JobCollection but use Iterator

--- src/Job/JobCollection.php
+++ src/Job/JobCollection.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace simpleQueue\Job;
 
-class JobCollection
+class JobCollection implements JobIterator
 {
     private array $items = [];
+
+    private int $position = 0;
 
     public function add(Job $job)
     {
@@ -19,5 +21,35 @@ class JobCollection
     public function all(): array
     {
         return $this->items;
+    }
+
+    public function getIterator()
+    {
+        return new \ArrayIterator($this->items);
+    }
+
+    public function current(): mixed
+    {
+        return $this->items[$this->position];
+    }
+
+    public function next(): void
+    {
+        $this->position++;
+    }
+
+    public function key(): int
+    {
+        return $this->position;
+    }
+
+    public function valid(): bool
+    {
+        return isset($this->items[$this->position]);
+    }
+
+    public function rewind(): void
+    {
+        $this->position = 0;
     }
 }
